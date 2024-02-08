25e6388327856d075f4a7b5f590f3ffa72ae6821
Victor <93835023+victor247k@users.noreply.github.com>
2024-02-08 20:52:49 +0200

    added color pallete and updated the element component

--- frontend/src/components/Element.jsx
+++ frontend/src/components/Element.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import Modal from "./Modal.jsx";
 import "./Element.css";
@@ -17,6 +17,10 @@ function Element({ symbol }) {
     }
   };
 
+  useEffect(() => {
+    handleClick()
+  }, [])
+
   const format_key = (key) => {
     let formatted = key.replace(/_/g, " "); 
     formatted = formatted.charAt(0).toUpperCase() + formatted.slice(1); 
@@ -25,54 +29,60 @@ function Element({ symbol }) {
 
   return (
     <>
-      <div onClick={() => setModal(true)} className="element">
-        <h1>{symbol}</h1>
-        <button onClick={handleClick}>Fetch Element</button> 
+      <div onClick={() => setModal(true) } className={`element ${elementData.category}`}>
+        <h2 className='element-symbol'>{symbol}</h2>
+        {elementData && (
+          <>
+            <span className='element-number'>{elementData.atomic_number}</span>
+            <span className='element-weight'>{elementData.atomic_weight}</span>
+            <span className='element-name'>{elementData.name}</span>
+          </>
+        )}
       </div>
       {modal && (
         <Modal openModal={modal} closeModal={() => setModal(false)}>
           {elementData ? (
-            <div className='element'>
+            <div className=''>
               <section>
                 <h2>{elementData.name}</h2>
                 <div>
-                  Number: {elementData.atomic_number} <br />
-                  Mass: {elementData.atomic_weight} <br />
-                  Electron configuration: {elementData.electron_configuration}  <br />
-                  Block: {elementData.block} <br />
-                  Category: {elementData.category} <br />
-                  Melting point: {elementData.melting_point} <br />
-                  Boiling point: {elementData.boiling_point} <br />
-                  Density: {elementData.density} <br />
+                  {elementData.atomic_number && <p>Number: {elementData.atomic_number}</p>}
+                  {elementData.atomic_weight && <p>Mass: {elementData.atomic_weight}</p>}
+                  {elementData.electron_configuration && <p>Electron configuration: {elementData.electron_configuration}</p>}
+                  {elementData.block && <p>Block: {elementData.block}</p>}
+                  {elementData.category && <p>Category: {elementData.category}</p>}
+                  {elementData.melting_point && <p>Melting point: {elementData.melting_point}</p>}
+                  {elementData.boiling_point && <p>Boiling point: {elementData.boiling_point}</p>}
+                  {elementData.density && <p>Density: {elementData.density}</p>}
                 </div>
               </section>
               <section>
-                Discovery: {elementData.discovery} <br />
-                Discovered in: {elementData.discovered_in} <br /> 
-                Appearance: {elementData.appearance} <br />
-                Abundance: {elementData.abundance} <br />
+                {elementData.discovery && <p>Discovery: {elementData.discovery}</p>}
+                {elementData.discovered_in && <p>Discovered in: {elementData.discovered_in}</p>}
+                {elementData.appearance && <p>Appearance: {elementData.appearance}</p>}
+                {elementData.abundance && <p>Abundance: {elementData.abundance}</p>}
               </section>
               <section>
                 <h3>Uses</h3>
                 <ul>
-                  {elementData.uses.map((v, i) => (
-                    <li key={i}>{v}</li> 
+                  {elementData.uses && elementData.uses.map((v, i) => (
+                    <li key={i}>{v}</li>
                   ))}
                 </ul>
               </section>
               <section>
                 <h3>Notable properties</h3>
                 <ul>
-                  {Object.entries(elementData.notable_properties).map(([key, value], i) => (
-                    <li key={i}>{format_key(key)}: {value}</li> 
+                  {elementData.notable_properties && Object.entries(elementData.notable_properties).map(([key, value], i) => (
+                    <li key={i}>{format_key(key)}: {value}</li>
                   ))}
                 </ul>
               </section>
               <section>
                 <h3>Biology facts</h3>
                 <ul>
-                  {Object.entries(elementData.biology_facts).map(([key, value], i) => (
-                    <li key={i}>{format_key(key)}: {value}</li> 
+                  {elementData.biology_facts && Object.entries(elementData.biology_facts).map(([key, value], i) => (
+                    <li key={i}>{format_key(key)}: {value}</li>
                   ))}
                 </ul>
               </section>
