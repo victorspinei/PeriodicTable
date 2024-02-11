598270d75ef82ea640eb1d2179922152aa214e50
Victor <93835023+victor247k@users.noreply.github.com>
2024-02-11 18:34:05 +0200

    stylled the modal a bit

--- frontend/src/components/Element.jsx
+++ frontend/src/components/Element.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import Modal from "./Modal.jsx";
 import "./Element.css";
-import "./Modal.css";
+import './ElementModal.css'
 import get_element from './api';
 
 function Element({ symbol, aditionalClass }) {
@@ -28,6 +28,10 @@ function Element({ symbol, aditionalClass }) {
     return formatted;
   }
 
+  const formatLink = () => {
+    return String(elementData.atomic_number).padStart(3, '0') + '_' + elementData.name.toLowerCase()
+  }
+
   return (
     <>
         {elementData && (
@@ -43,25 +47,30 @@ function Element({ symbol, aditionalClass }) {
       {modal && (
         <Modal className='modal' openModal={modal} closeModal={() => setModal(false)}>
           {elementData ? (
-            <div className=''>
+            <div className='modal-div'>
               <section>
-                <h2 className='modal-name'>{elementData.name}</h2>
-                <div>
-                  {elementData.atomic_number && <p>Number: {elementData.atomic_number}</p>}
-                  {elementData.atomic_weight && <p>Mass: {elementData.atomic_weight}</p>}
-                  {elementData.electron_configuration && <p>Electron configuration: {elementData.electron_configuration}</p>}
-                  {elementData.block && <p>Block: {elementData.block}</p>}
-                  {elementData.category && <p>Category: {elementData.category}</p>}
-                  {elementData.melting_point && <p>Melting point: {elementData.melting_point}</p>}
-                  {elementData.boiling_point && <p>Boiling point: {elementData.boiling_point}</p>}
-                  {elementData.density && <p>Density: {elementData.density}</p>}
+                <h2 className={`modal-name ${aditionalClass}`}>{elementData.name}</h2>
+                <div className='modal-basic-data'>
+                  {elementData.atomic_number && <p>Number: <span className={aditionalClass}>{elementData.atomic_number}</span></p>}
+                  {elementData.atomic_weight && <p>Mass: <span className={aditionalClass}>{elementData.atomic_weight}</span></p>}
+                  {elementData.electron_configuration && <p>Electron configuration: <span className={aditionalClass}>{elementData.electron_configuration}</span></p>}
+                  {elementData.block && <p>Block: <span className={aditionalClass}>{elementData.block}</span></p>}
+                  {elementData.category && <p>Category: <span className={aditionalClass}>{elementData.category}</span></p>}
+                  {elementData.melting_point && <p>Melting point: <span className={aditionalClass}>{elementData.melting_point}</span></p>}
+                  {elementData.boiling_point && <p>Boiling point: <span className={aditionalClass}>{elementData.boiling_point}</span></p>}
+                  {elementData.density && <p>Density: <span className={aditionalClass}>{elementData.density}</span></p>}
                 </div>
               </section>
-              <section>
+              <section className='model-extra'>
+              <video autoPlay={true} loop>
+                <source src={`https://www.gstatic.com/culturalinstitute/searchar/assets/element_${formatLink()}/desktop_dark.mp4`} />
+              </video>
+              <div>
                 {elementData.discovery && <p>Discovery: {elementData.discovery}</p>}
                 {elementData.discovered_in && <p>Discovered in: {elementData.discovered_in}</p>}
                 {elementData.appearance && <p>Appearance: {elementData.appearance}</p>}
                 {elementData.abundance && <p>Abundance: {elementData.abundance}</p>}
+              </div>
               </section>
               <section>
                 <h3>Uses</h3>
